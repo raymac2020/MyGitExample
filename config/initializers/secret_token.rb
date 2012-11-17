a17f03e044f8a4e7b1ebea0e0f26e261d1a39b04
raymac2020 <ray.mcelhinney@gmail.com>
2012-11-17 10:41:38 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyGitExample::Application.config.secret_token = 'c19af103d94810889f34d901468b52f897ff623ccff13eea1845284430bae07e3f3623031cdf804da2e95fb215f03bf7a07622ce8f5449ddce07b1a3ab459cc1'
